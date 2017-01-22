4c47560f46d01860a2b32aa7ec7a13bcbacd2064
AdamSturge <AdamSturge991@hotmail.com>
2017-01-22 15:00:10 -0500

    implemented fd_partial_derivative, fd_grad, fd_interpolate

--- src/fd_grad.cpp
+++ src/fd_grad.cpp
@@ -1,4 +1,5 @@
 #include "fd_grad.h"
+#include "fd_partial_derivative.h"
 
 void fd_grad(
   const int nx,
@@ -7,7 +8,31 @@ void fd_grad(
   const double h,
   Eigen::SparseMatrix<double> & G)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+	Eigen::SparseMatrix<double> Dx((nx-1)*ny*nz,nx*ny*nz);
+	Eigen::SparseMatrix<double> Dy(nx*(ny-1)*nz, nx*ny*nz);
+	Eigen::SparseMatrix<double> Dz(nx*ny*(nz-1), nx*ny*nz);
+
+	fd_partial_derivative(nx, ny, nz, h, 0, Dx);
+	fd_partial_derivative(nx, ny, nz, h, 1, Dy);
+	fd_partial_derivative(nx, ny, nz, h, 2, Dz);
+
+	/* This seems like it should work but it will not compile 
+	G.topRows((nx - 1)*ny*nz) = Dx;
+	G.middleRows((nx-1)*ny*nz, nx*(ny-1)*nz) = Dy;
+	G.bottomRows(nx*ny*(nz-1)) = Dz;
+	*/
+
+	for (int c = 0; c < nx*ny*nz; ++c) {
+		for (Eigen::SparseMatrix<double>::InnerIterator it(Dx, c); it; ++it) {
+			G.insertBack(it.row(), c) = it.value();
+		}
+		for (Eigen::SparseMatrix<double>::InnerIterator it(Dy, c); it; ++it) {
+			G.insertBack(it.row(), c) = it.value();
+		}
+		for (Eigen::SparseMatrix<double>::InnerIterator it(Dz, c); it; ++it) {
+			G.insertBack(it.row(), c) = it.value();
+		}
+	}
+	G.finalize();
+
 }
--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -8,7 +8,44 @@ void fd_partial_derivative(
   const int dir,
   Eigen::SparseMatrix<double> & D)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+	//O((nx*ny*nz)^2)
+	std::vector<Eigen::Triplet<double>> tripletList;
+	tripletList.reserve(nx*ny*nz);
+
+	int iStart = 0;
+	int jStart = 0;
+	int kStart = 0;
+	switch (dir) {
+		case 0: iStart = 1; break;
+		case 1: jStart = 1; break;
+		case 3: kStart = 1; break;
+		default: break;
+	}
+
+	for (int i = iStart; i < nx; ++ i) {
+		for (int j = jStart; j < ny; ++j) {
+			for (int k = kStart; k < nz; ++k) {
+				int row = i + j*nx + k*nx*ny;
+				for (int l = 0; l < nx*ny*nz; ++l) {
+					int dirIndex = 0;
+					switch (dir) {
+						case 0:  dirIndex = i; break;
+						case 1:  dirIndex = j; break;
+						case 2:  dirIndex = k; break;
+						default: dirIndex = i; break;
+					}
+					
+					if (l == dirIndex - 1) {
+						tripletList.push_back(Eigen::Triplet<double>(row, l, -1/h));
+					}
+					else if(l == dirIndex) {
+						tripletList.push_back(Eigen::Triplet<double>(row, l, 1/h));
+					}
+				}
+			}
+		}
+	}
+	D.setFromTriplets(tripletList.begin(), tripletList.end());
+	D.makeCompressed();
+	
 }
