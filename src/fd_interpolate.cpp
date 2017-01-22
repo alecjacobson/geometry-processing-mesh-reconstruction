a728f9ad18b6dbff14d07eef387d4f15260304e1
leng <maryelaine.malit@gmail.com>
2017-01-22 15:09:25 -0500

    implemented partial_derivative; scratching interpolation

--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -14,15 +14,7 @@ void fd_interpolate(
   W.resize(P.rows(), nx*ny*nz);
   int pi, pj, pk, col;
   
-  // Step over each point in P and find corresponding subscripts
-  // on the grid
   for(int p = 0; p < P.rows(); p++) {
-    pi = (P(p, 0) - corner(0,0)) / h; // i
-    pj = (P(p, 1) - corner(0,1)) / h; // j
-    pk = (P(p, 2) - corner(0,2)) / h; // k
     
-    // set value in W to indicate node is in P
-    col = pi + nx*(pj + pk * ny);
-    W.insert(p, col) = 1;
   }
 }
--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -8,7 +8,40 @@ void fd_partial_derivative(
   const int dir,
   Eigen::SparseMatrix<double> & D)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  int ind, i, j, k;
+  
+  if (dir == 0) {
+    D.resize( (nx - 1)*ny*nz, nx*ny*nz );
+    for (i = 1; i < nx; i++) {
+      for (j = 0; j < ny; j++) {
+        for (k = 0; k < nz; k++) {
+          ind = i + nx*(j + k * ny);
+          D.insert(ind, i-1) = -1 / h;
+          D.insert(ind, i) = 1 / h;
+        }
+      }
+    }
+  } else if (dir == 1) {
+    D.resize( nx*(ny-1)*nz, nx*ny*nz );
+    for (i = 0; i < nx; i++) {
+      for (j = 1; j < ny; j++) {
+        for (k = 0; k < nz; k++) {
+          ind = i + nx*(j + k * ny);
+          D.insert(ind, j-1) = -1 / h;
+          D.insert(ind, j) = 1 / h;
+        }
+      }
+    }
+  } else {
+    D.resize( nx*ny*(nz-1), nx*ny*nz );
+    for (i = 0; i < nx; i++) {
+      for (j = 0; j < ny; j++) {
+        for (k = 1; k < nz; k++) {
+          ind = i + nx*(j + k * ny);
+          D.insert(ind, k-1) = -1 / h;
+          D.insert(ind, k) = 1 / h;
+        }
+      }
+    }
+  }
 }
