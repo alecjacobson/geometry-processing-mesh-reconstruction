c267579d9b72fc0b5b84fbee526108d39357cfa9
rkevingibson <rkevingibson@gmail.com>
2017-01-23 11:24:23 -0500

    Partial derivative implementation done. Should work.

--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -8,7 +8,19 @@ void fd_partial_derivative(
   const int dir,
   Eigen::SparseMatrix<double> & D)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+	const int ns[3]{ nx, ny, nz };
+
+	//Do we need to worry about overflow?
+	unsigned int m = nx*ny*nz / ns[dir]; //This is equivalent to (nx-1)*ny*nz when dir=0, etc.
+	D.resize(m, nx*ny*nz);
+
+	std::vector<Eigen::Triplet<double>> entries;
+	entries.reserve(2 * m);
+
+	for (int i = 0; i < m; i++) {
+		entries.emplace_back(i, i + 1, -1);
+		entries.emplace_back(i, i, 1);
+	}
+
+	D.setFromTriplets(entries.cbegin(), entries.cend());
 }
