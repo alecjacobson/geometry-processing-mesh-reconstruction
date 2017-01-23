dca1e28a6da2b258d0a4d40e7bc5a294805881bf
rkevingibson <rkevingibson@gmail.com>
2017-01-22 22:32:37 -0500

    First draft of fd_interpolate. I think it's correct, barring some Matrix bookkeeping. I think I need to resize the output matrix W

--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -1,15 +1,43 @@
 #include "fd_interpolate.h"
 
+#include <cmath>
 void fd_interpolate(
-  const int nx,
-  const int ny,
-  const int nz,
-  const double h,
-  const Eigen::RowVector3d & corner,
-  const Eigen::MatrixXd & P,
-  Eigen::SparseMatrix<double> & W)
+	const int nx,
+	const int ny,
+	const int nz,
+	const double h,
+	const Eigen::RowVector3d & corner,
+	const Eigen::MatrixXd & P,
+	Eigen::SparseMatrix<double> & W)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+	
+	std::vector<Eigen::Triplet<double>> entries;
+	for (int i = 0; i < P.rows(); i++) {
+		auto grid_point = (P.row(i) - corner) / h;
+
+		double weights[3][2];
+		Eigen::RowVector3i index;
+		for (int j = 0; j < 3; j++) {
+			double integral;
+			double frac = std::modf(grid_point(j), &integral);
+
+			index(j) = (int)integral;
+			weights[j][0] = 1.0 - frac;
+			weights[j][1] = frac;
+		}
+
+		for (int x = 0; x <= 1; x++) {
+			for (int y = 0; y <= 1; y++) {
+				for (int z = 0; z <= 1; z++) {
+					const int col = index.x() + x + (index.y() + y)*nx + (index.z() + z)*nx*ny;
+					entries.emplace_back(
+						i,
+						col,
+						weights[0][x] * weights[1][y] * weights[1][z]);
+				}
+			}
+		}
+	}
+
+	W.setFromTriplets(entries.begin(), entries.end());
 }
