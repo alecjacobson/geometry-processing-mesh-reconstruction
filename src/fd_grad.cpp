c2142f6045ac9ee32e9f008a784a8d2fe9f07ded
GavinBarill <gavinpcb@gmail.com>
2018-01-27 19:50:19 -0500

    done

--- src/fd_grad.cpp
+++ src/fd_grad.cpp
@@ -1,4 +1,6 @@
 #include "fd_grad.h"
+#include "fd_partial_derivative.h"
+#include <iostream>
 
 void fd_grad(
   const int nx,
@@ -10,4 +12,13 @@ void fd_grad(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+    int m = nx*ny*nz;
+    Eigen::SparseMatrix<double> Dx((nx-1)*ny*nz,m), Dy((ny-1)*nx*nz,m), Dz((nz-1)*ny*nx,m);
+    fd_partial_derivative(nx,ny,nz,h,0,Dx);
+    fd_partial_derivative(nx,ny,nz,h,1,Dy);
+    fd_partial_derivative(nx,ny,nz,h,2,Dz);
+    
+    Eigen::SparseMatrix<double> C(Dx.rows()+Dy.rows(),Dx.cols());
+    igl::cat(1,Dx,Dy,C);
+    igl::cat(1,C,Dz,G);
 }
--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -12,4 +12,37 @@ void fd_interpolate(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+    int n = P.rows();
+    
+    typedef Eigen::Triplet<double> T;
+    std::vector<T> tripletList;
+    tripletList.reserve(n*8);
+    
+    for(int i = 0; i < n; i++){
+        double coord = P(i,0) - corner(0);
+        double mod = coord/h;
+        int floorx = int(mod);
+        double weightx = 1-(mod - floorx);
+        
+        coord = P(i,1) - corner(1);
+        mod = coord/h;
+        int floory = int(mod);
+        double weighty = 1-(mod - floory);
+        
+        coord = P(i,2) - corner(2);
+        mod = coord/h;
+        int floorz = int(mod);
+        double weightz = 1-(mod - floorz);
+        
+        tripletList.push_back(T(i,floorx + nx*floory + nx*ny*floorz,weightx*weighty*weightz));
+        tripletList.push_back(T(i,floorx+1 + nx*floory + nx*ny*floorz,(1-weightx)*weighty*weightz));
+        tripletList.push_back(T(i,floorx + nx*(floory+1) + nx*ny*floorz,weightx*(1-weighty)*weightz));
+        tripletList.push_back(T(i,floorx+1 + nx*(floory+1) + nx*ny*floorz,(1-weightx)*(1-weighty)*weightz));
+        
+        tripletList.push_back(T(i,floorx + nx*floory + nx*ny*(floorz+1),weightx*weighty*(1-weightz)));
+        tripletList.push_back(T(i,floorx+1 + nx*floory + nx*ny*(floorz+1),(1-weightx)*weighty*(1-weightz)));
+        tripletList.push_back(T(i,floorx + nx*(floory+1) + nx*ny*(floorz+1),weightx*(1-weighty)*(1-weightz)));
+        tripletList.push_back(T(i,floorx+1 + nx*(floory+1) + nx*ny*(floorz+1),(1-weightx)*(1-weighty)*(1-weightz)));
+    }
+    W.setFromTriplets(tripletList.begin(), tripletList.end());
 }
