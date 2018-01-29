3f8ff0190d835f23bb5a79ec8a23b5b3f7f41e36
Yanjun7J <y7jiang@gmail.com>
2018-01-29 18:04:55 -0500

    A1

--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -12,4 +12,39 @@ void fd_interpolate(
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+	using namespace std;
+	typedef Eigen::Triplet<double> T;
+	vector<T> triplets;
+	triplets.reserve(P.rows()*8*2);
+	for (int row = 0; row < P.rows(); row++) {
+		double dx = P(row, 0) - corner(0);
+		double dy = P(row, 1) - corner(1);
+		double dz = P(row, 2) - corner(2);
+		int i = int(floor(dx / h));
+		double x_value = fmod(dx, h) / h;
+		int j = int(floor(dy / h));
+		double y_value = fmod(dy, h) / h;
+		int k = int(floor(dz / h));
+		double z_value = fmod(dz, h) / h;
+
+		// compute the value for the 8 neighboring nodes
+		triplets.push_back(T(row, i + nx * (j + k * ny), 
+			(1.0 - x_value) * (1.0 - y_value) * (1.0 - z_value)));
+		triplets.push_back(T(row, (i + 1) + nx * (j + k * ny),
+			x_value * (1.0 - y_value) * (1.0 - z_value)));
+		triplets.push_back(T(row, i + nx * ((j + 1) + k * ny),
+			(1.0 - x_value) * y_value * (1.0 - z_value)));
+		triplets.push_back(T(row, (i + 1) + nx * ((j + 1) + k * ny),
+			x_value * y_value * (1.0 - z_value)));
+		triplets.push_back(T(row, i + nx * (j + (k + 1) * ny),
+			(1.0 - x_value) * (1.0 - y_value) * z_value));
+		triplets.push_back(T(row, (i + 1) + nx * (j + (k + 1) * ny),
+			x_value * (1.0 - y_value) * z_value));
+		triplets.push_back(T(row, i + nx * ((j + 1) + (k + 1) * ny),
+			(1.0 - x_value) * y_value * z_value));
+		triplets.push_back(T(row, (i + 1) + nx * ((j + 1) + (k + 1) * ny),
+			x_value * y_value * z_value));
+	}
+	W.resize(P.rows(), nx*ny*nz);
+	W.setFromTriplets(triplets.begin(), triplets.end());
 }
--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -1,14 +1,46 @@
 #include "fd_partial_derivative.h"
 
 void fd_partial_derivative(
-  const int nx,
-  const int ny,
-  const int nz,
-  const double h,
-  const int dir,
-  Eigen::SparseMatrix<double> & D)
+	const int nx,
+	const int ny,
+	const int nz,
+	const double h,
+	const int dir,
+	Eigen::SparseMatrix<double> & D)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////
+	// Add your code here
+	////////////////////////////////////////////////////////////////////////////
+	typedef Eigen::Triplet<double> T;
+	std::vector<T> triplets;
+	triplets.reserve(nx*ny*nz*4);
+	int x_num = nx, y_num = ny, z_num = nz;
+	int ix = 0, jy = 0, kz = 0;
+	if (dir == 0) {// partial derivative with regard to x
+		x_num--; // set number of grid nodes in direction x
+		ix = 1; // set the difference in x direction
+	}
+	else if (dir == 1) {// partial derivative with regard to y
+		y_num--; // set number of grid nodes in direction y
+		jy = 1; // set the difference in y direction
+	}
+	else {// dir == 2 // partial derivative with regard to z
+		z_num--; // set number of grid nodes in direction z
+		kz = 1; // set the difference in z direction
+	}
+	double dif = 1.0 / h;
+	// compute the partial derivative matrix
+	for (int i = 0; i < x_num; i++) {
+		for (int j = 0; j < y_num; j++) {
+			for (int k = 0; k < z_num; k++) {
+				int row = i + x_num * (j + k * y_num);
+				int col1 = i + nx * (j + k * ny);
+				int col2 = i + ix + nx * ((j + jy) + (k + kz) * ny);
+				triplets.push_back(T(row, col1, -dif));
+				triplets.push_back(T(row, col2, dif));
+			}
+		}
+	}
+	D.resize(x_num*y_num*z_num, nx*ny*nz);
+	D.setFromTriplets(triplets.begin(), triplets.end());
 }
