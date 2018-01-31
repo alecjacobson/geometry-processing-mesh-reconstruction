653186278a926d4be6beb6dd3b69a5d2ce63d786
sajjadn <nezhadi.sajjad@gmail.com>
2018-01-30 23:20:05 -0500

    Delayed but finished

--- src/fd_grad.cpp
+++ src/fd_grad.cpp
@@ -1,4 +1,6 @@
 #include "fd_grad.h"
+#include "fd_partial_derivative.h"
+#include "igl/cat.h"
 
 void fd_grad(
   const int nx,
@@ -7,7 +9,14 @@ void fd_grad(
   const double h,
   Eigen::SparseMatrix<double> & G)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  Eigen::SparseMatrix<double> D1;
+  Eigen::SparseMatrix<double> D2;
+  Eigen::SparseMatrix<double> D3;
+
+  fd_partial_derivative(nx, ny, nz, h, 0, D1);
+  fd_partial_derivative(nx, ny, nz, h, 1, D2);
+  fd_partial_derivative(nx, ny, nz, h, 2, D3);
+
+  G = igl::cat(1, D1, D2);
+  G = igl::cat(1, G, D3);
 }
--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -1,4 +1,5 @@
 #include "fd_interpolate.h"
+#include <iostream>
 
 void fd_interpolate(
   const int nx,
@@ -9,7 +10,30 @@ void fd_interpolate(
   const Eigen::MatrixXd & P,
   Eigen::SparseMatrix<double> & W)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  W.resize(P.rows(), nx*ny*nz);
+  typedef Eigen::Triplet<double> T;
+  std::vector<T> tripletList;
+  tripletList.reserve(P.rows()*8);
+
+  for(int i = 0; i < P.rows(); i += 1){
+
+    int x = floor((P(i, 0) - corner(0))/h);
+    int y = floor((P(i, 1) - corner(1))/h);
+    int z = floor((P(i, 2) - corner(2))/h);
+
+    double dx = (P(i, 0) - corner(0))/h - x;
+    double dy = (P(i, 1) - corner(1))/h - y;
+    double dz = (P(i, 2) - corner(2))/h - z;
+
+    tripletList.push_back(T(i, x + nx*y + nx*ny*z, (1-dx)*(1-dy)*(1-dz)));
+    tripletList.push_back(T(i, x + nx*y + nx*ny*(z+1), (1-dx)*(1-dy)*dz));
+    tripletList.push_back(T(i, x + nx*(y+1) + nx*ny*z, (1-dx)*dy*(1-dz)));
+    tripletList.push_back(T(i, x + nx*(y+1) + nx*ny*(z+1), (1-dx)*dy*dz));
+    tripletList.push_back(T(i, (x+1) + nx*y + nx*ny*z, dx*(1-dy)*(1-dz)));
+    tripletList.push_back(T(i, (x+1) + nx*y + nx*ny*(z+1), dx*(1-dy)*dz));
+    tripletList.push_back(T(i, (x+1) + nx*(y+1) + nx*ny*z, dx*dy*(1-dz)));
+    tripletList.push_back(T(i, (x+1) + nx*(y+1) + nx*ny*(z+1), dx*dy*dz));
+
+  }
+  W.setFromTriplets(tripletList.begin(), tripletList.end());
 }
