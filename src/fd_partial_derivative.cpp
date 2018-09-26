3ce762d25375cb59cb99044243fab57958225c6b
Simeon Krastnikov <simeon.krastnikov@mail.utoronto.ca>
2018-09-26 12:10:05 -0400

    HW1 Submission

--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -1,4 +1,5 @@
 #include "fd_partial_derivative.h"
+#include <vector>
 
 void fd_partial_derivative(
   const int nx,
@@ -8,7 +9,26 @@ void fd_partial_derivative(
   const int dir,
   Eigen::SparseMatrix<double> & D)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  int nx_p = nx - (dir == 0);
+  int ny_p = ny - (dir == 1);
+  int nz_p = nz - (dir == 2);
+
+  D.resize(nx_p*ny_p*nz_p, nx*ny*nz);
+  
+  typedef Eigen::Triplet<double> T;
+  std::vector<T> tripletList;
+  
+  for (int i = 0; i < nx_p; i++) {
+    for (int j = 0; j < ny_p; j++) {
+      for (int k = 0; k < nz_p; k++) {
+        int row = i + nx_p*(j + k * ny_p);
+        int left_ind = i + nx*(j + k * ny);
+        int right_ind = i + (dir == 0) + nx*(j + (dir == 1) + (k + (dir == 2))* ny);
+        tripletList.push_back(T(row,left_ind,-1/h));
+        tripletList.push_back(T(row,right_ind,1/h));
+      }
+    }
+  }
+  
+  D.setFromTriplets(tripletList.begin(), tripletList.end());
 }
