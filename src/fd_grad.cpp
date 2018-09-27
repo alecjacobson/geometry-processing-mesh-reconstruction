08e82f5bfd4b0d1c9afd1c8560b62c7770bd9174
Ruijian An <rayruijian@gmail.com>
2018-09-27 14:44:53 -0400

    hw01

--- src/fd_grad.cpp
+++ src/fd_grad.cpp
@@ -1,4 +1,6 @@
 #include "fd_grad.h"
+#include "fd_partial_derivative.h"
+#include <igl/cat.h>
 
 void fd_grad(
   const int nx,
@@ -7,7 +9,16 @@ void fd_grad(
   const double h,
   Eigen::SparseMatrix<double> & G)
 {
-  ////////////////////////////////////////////////////////////////////////////
-  // Add your code here
-  ////////////////////////////////////////////////////////////////////////////
+  Eigen::SparseMatrix<double>
+  Gx((nx-1)*ny*nz, nx * ny * nz),
+  Gy(nx*(ny-1)*nz, nx * ny * nz),
+  Gz(nx*ny*(nz-1), nx * ny * nz),
+  result;
+
+  fd_partial_derivative(nx, ny, nz, h, 0, Gx);
+  fd_partial_derivative(nx, ny, nz, h, 1, Gy);
+  fd_partial_derivative(nx, ny, nz, h, 2, Gz);
+
+  igl::cat(1, Gx, Gy, result);
+  igl::cat(1, result, Gz, G);
 }
