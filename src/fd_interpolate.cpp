a74c36a0d0ff4a700d72cf240d24c62e749e8140
liupeiLu <liupei.lu@mail.utoronto.ca>
2018-09-27 23:40:13 -0400

    Assignment 1 Liupei Lu

--- src/fd_interpolate.cpp
+++ src/fd_interpolate.cpp
@@ -1,15 +1,94 @@
 #include "fd_interpolate.h"
+#include <iostream>
+#include <math.h>
+
+typedef Eigen::Triplet<double> T;
+
+//lookup table
+//http://paulbourke.net/miscellaneous/interpolation/
+double get_coefficient(double dx, double dy, double dz, int corner)
+{
+  switch (corner)
+  {
+  case 0: //C000
+    return (1 - dx) * (1 - dy) * (1 - dz);
+  case 1: //C100
+    return dx * (1 - dy) * (1 - dz);
+  case 2: //C010
+    return (1 - dx) * dy * (1 - dz);
+  case 3: //C001
+    return (1 - dx) * (1 - dy) * dz;
+  case 4: //C101
+    return dx * (1 - dy) * dz;
+  case 5: //C011
+    return (1 - dx) * dy * dz;
+  case 6: //C110
+    return dx * dy * (1 - dz);
+  case 7: //C111
+    return dx * dy * dz;
+  }
+}
 
 void fd_interpolate(
-  const int nx,
-  const int ny,
-  const int nz,
-  const double h,
-  const Eigen::RowVector3d & corner,
-  const Eigen::MatrixXd & P,
-  Eigen::SparseMatrix<double> & W)
+    const int nx,
+    const int ny,
+    const int nz,
+    const double h,
+    const Eigen::RowVector3d &corner,
+    const Eigen::MatrixXd &P,
+    Eigen::SparseMatrix<double> &W)
 {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+
+  //std::cout <<"corner is :"<< corner << std::endl;
+  W.resize(P.rows(), nx * ny * nz);
+
+  std::vector<T> tlist;
+  tlist.reserve(8 * P.rows());
+
+  for (int i = 0; i < P.rows(); i++)
+  {
+    double distance[] = {0.0, 0.0, 0.0};
+    int grid[] = {0, 0, 0};
+    double d[] = {0.0, 0.0, 0.0};
+
+    for (int j = 0; j < 3; j++)
+    {
+      distance[j] = P(i, j) - corner[j];
+      grid[j] = floor(distance[j] / h);
+      d[j] = (distance[j] - grid[j] * h) / h;
+    }
+
+    tlist.push_back(T(i, grid[0] + grid[1] * nx + grid[2] * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 0)));
+    tlist.push_back(T(i, (grid[0] + 1) + grid[1] * nx + grid[2] * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 1)));
+    tlist.push_back(T(i, grid[0] + (grid[1] + 1) * nx + grid[2] * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 2)));
+    tlist.push_back(T(i, grid[0] + grid[1] * nx + (grid[2] + 1) * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 3)));
+    tlist.push_back(T(i, (grid[0] + 1) + grid[1] * nx + (grid[2] + 1) * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 4)));
+    tlist.push_back(T(i, grid[0] + (grid[1] + 1) * nx + (grid[2] + 1) * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 5)));
+    tlist.push_back(T(i, (grid[0] + 1) + grid[1] * nx + (grid[2] + 1) * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 6)));
+    tlist.push_back(T(i, (grid[0] + 1) + (grid[1] + 1) * nx + (grid[2] + 1) * nx * ny,
+                      get_coefficient(d[0], d[1], d[2], 7)));
+
+    /*
+    W.coeffRef(i, grid[0] + grid[1] * nx + grid[2] * nx * ny) = get_coefficient(d[0],d[1],d[2], 0);
+    W.coeffRef(i, (grid[0] + 1) + grid[1]* nx + grid[2] * nx * ny) = get_coefficient(d[0],d[1],d[2], 1);
+    W.coeffRef(i, grid[0] + (grid[1] + 1) * nx + grid[2] * nx * ny) = get_coefficient(d[0],d[1],d[2], 2);
+    W.coeffRef(i, grid[0] + grid[1] * nx + (grid[2] + 1) * nx * ny) = get_coefficient(d[0],d[1],d[2], 3);
+    W.coeffRef(i, (grid[0] + 1) + grid[1] * nx + (grid[2] + 1) * nx * ny) = get_coefficient(d[0],d[1],d[2], 4);
+    W.coeffRef(i, grid[0] + (grid[1] + 1) * nx + (grid[2] + 1) * nx * ny) = get_coefficient(d[0],d[1],d[2], 5);
+    W.coeffRef(i, (grid[0] + 1) + grid[1] * nx + (grid[2] +1)* nx * ny) = get_coefficient(d[0],d[1],d[2], 6);
+    W.coeffRef(i, (grid[0] + 1) + (grid[1] + 1) * nx + (grid[2] + 1) * nx * ny) = get_coefficient(d[0],d[1],d[2], 7);*/
+  }
+
+  //W.makeCompressed();
+  W.setFromTriplets(tlist.begin(), tlist.end());
 }
--- src/fd_partial_derivative.cpp
+++ src/fd_partial_derivative.cpp
@@ -1,14 +1,70 @@
 #include "fd_partial_derivative.h"
+typedef Eigen::Triplet<double> T;
 
 void fd_partial_derivative(
-  const int nx,
-  const int ny,
-  const int nz,
-  const double h,
-  const int dir,
-  Eigen::SparseMatrix<double> & D)
+    const int nx,
+    const int ny,
+    const int nz,
+    const double h,
+    const int dir,
+    Eigen::SparseMatrix<double> &D)
 {
   ////////////////////////////////////////////////////////////////////////////
   // Add your code here
   ////////////////////////////////////////////////////////////////////////////
+
+  int m = 0;
+  int space = 0;
+
+  switch (dir)
+  {
+  case 0:
+    m = (nx - 1) * ny * nz;
+    space = 1;
+    break;
+
+  case 1:
+    m = nx * (ny - 1) * nz;
+    space = nx;
+    break;
+
+  case 2:
+    m = nx * ny * (nz - 1);
+    space = nx * ny;
+    break;
+  }
+
+  D.resize(m, nx * ny * nz);
+
+  std::vector<T> tlist;
+  tlist.reserve(2 * m);
+
+  int count = 0;
+
+  for (int k = 0; k < nz; k++)
+  {
+    for (int j = 0; j < ny; j++)
+    {
+      for (int i = 0; i < nx; i++)
+      {
+        int index = i + j * nx + k * ny * nx;
+        int col = index;
+        int row = count;
+
+        if ((i == nx - 1 && dir == 0) || (j == ny - 1 && dir == 1) || (k == nz - 1 && dir == 2))
+        {
+          //skip where index reach last point in a loop
+          continue;
+        }
+        else
+        {
+          tlist.push_back(T(row, col, -1.0 / h));
+          tlist.push_back(T(row, col + space, 1.0 / h));
+          count++;
+        }
+      }
+    }
+  }
+
+  D.setFromTriplets(tlist.begin(), tlist.end());
 }
